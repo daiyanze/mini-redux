a34c5841906fee12e5209f92dabfcd1d6c6deae6
Yanze <ginoalex8964@yahoo.com>
2020-08-03 18:22:26 +0900

    chore: add build

---
+++ build/precache-manifest.e5871a08ed80a7e6684732ed204f46c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da5379629d7707c6c5acf6630555372",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85aaa81a78b9038acd80",
+    "url": "./static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "d7013a44e930234558bb",
+    "url": "./static/js/2.633c6d55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.633c6d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85aaa81a78b9038acd80",
+    "url": "./static/js/main.28ca72a1.chunk.js"
+  },
+  {
+    "revision": "d3aa5bb1fd6829f08ad6",
+    "url": "./static/js/runtime-main.d831a2e9.js"
+  }
+]);
